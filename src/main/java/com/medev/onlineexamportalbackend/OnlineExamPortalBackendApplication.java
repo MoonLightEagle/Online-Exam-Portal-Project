cc4a0dfed4bdaa851af37a2037d6750445b9fc0a
MoonLightEagle <danylosokoliuk13@gmail.com>
2024-01-27 16:18:53 +0200

    initial project setup

---
+++ src/main/java/com/medev/onlineexamportalbackend/OnlineExamPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medev.onlineexamportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamPortalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamPortalBackendApplication.class, args);
+    }
+
+}

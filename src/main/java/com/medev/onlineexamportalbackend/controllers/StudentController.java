6595d221dc198ce522b474f9b0d74698881a686d
MoonLightEagle <danylosokoliuk13@gmail.com>
2024-01-28 19:46:29 +0200

    deleted unused class studentserviceimpl , updates studentcontrolled endpoints

--- src/main/java/com/medev/onlineexamportalbackend/controllers/StudentController.java
+++ src/main/java/com/medev/onlineexamportalbackend/controllers/StudentController.java
@@ -1,6 +1,7 @@
 package com.medev.onlineexamportalbackend.controllers;
 
 import com.medev.onlineexamportalbackend.entity.Student;
+
 import com.medev.onlineexamportalbackend.service.StudentService;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +12,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
-    private  final StudentService studentService;
+    private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
 
     @GetMapping
     public List<Student> getAllStudents(@RequestParam(name="grade", required = false) Long grade){
@@ -34,8 +36,6 @@ public class StudentController {
         }
     }
 
-
-
     @PostMapping
     public Student postStudent(@RequestBody Student student){
         return studentService.postStudent(student);
@@ -43,13 +43,13 @@ public class StudentController {
 
     @PutMapping("/{id}")
     public Student putStudentById(@PathVariable Long id, @RequestBody Student student){
-        // TODO endpoint to put data by studentId
-        return studentService.postStudent(student);
+
+        return studentService.putStudentById(id,student);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteStudentById(@PathVariable Long id){
-        // TODO endpoint to delete student by studentId
+    public Student deleteStudentById(@PathVariable Long id){
+        return studentService.deleteStudentById(id);
     }
 
 }
--- src/main/java/com/medev/onlineexamportalbackend/service/StudentService.java
+++ src/main/java/com/medev/onlineexamportalbackend/service/StudentService.java
@@ -1,27 +1,68 @@
 package com.medev.onlineexamportalbackend.service;
 
 import com.medev.onlineexamportalbackend.entity.Student;
+import com.medev.onlineexamportalbackend.repository.StudentRepository;
 import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
 
-public interface StudentService {
-
-    public List<Student> getAllStudents(Long grade);
-
-    public Student getStudentById( Long id) throws BadRequestException;
-
-    public List<Student> getStudentsByExamId( Long id);
-
-
-    public Student postStudent( Student student);
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
 
-    public Student putStudentById( Long id,  Student student);
+    public List<Student> getAllStudents(Long grade) {
+        if (grade!=null){
+            return studentRepository.findAllByGrade(grade);
+        }else{
+            return studentRepository.findAll();
+        }
+
+    }
 
 
-    public void deleteStudentById( Long id);
+
+    public Student getStudentById(Long id) throws BadRequestException {
+        return studentRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Student with ID " + id + " not found"));
+    }
+
+    public List<Student> getStudentsByExamId(Long id) {
+        return studentRepository.findByExams(id);
+    }
+
+
+    public Student postStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+
+    public Student putStudentById(Long id, Student student) {
+        Student oldStudent = null;
+        if(studentRepository.findById(id).isPresent()){
+            oldStudent = studentRepository.findById(id).get();
+        }else{
+            // TODO need to return sort of error
+            return null;
+        }
+        oldStudent.setEmail(student.getEmail());
+        oldStudent.setGrade(student.getGrade());
+        oldStudent.setContactPhone(student.getContactPhone());
+        oldStudent.setFirstName(student.getFirstName());
+        oldStudent.setLastName(student.getLastName());
+        return oldStudent;
+
+    }
+
+
+    public Student deleteStudentById(Long id) {
+        var oldStudent = studentRepository.findById(id).get();
+        studentRepository.deleteById(id);
+        return oldStudent;
+    }
 }
